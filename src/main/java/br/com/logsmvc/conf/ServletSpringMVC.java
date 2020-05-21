7e99913377325058dae1c581a744c95a0b636810
DiegoCSantos <d.lessus@gmail.com>
2020-05-21 15:24:52 -0300

    Estrutura do projeto

---
+++ src/main/java/br/com/logsmvc/conf/ServletSpringMVC.java
@@ -0,0 +1,23 @@
+package br.com.logsmvc.conf;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new  Class[] {AppWebConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
